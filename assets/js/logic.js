d13bdc9e28a26ab0f78676848e418a20e32ac3ad
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-01-13 11:33:12 +0000

    Added JS files based on filename hints in index.html & started writing psuedo code

---
+++ assets/js/logic.js
@@ -0,0 +1,36 @@
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+// Acceptance Criteria
+// Create a code quiz that contains the following requirements:
+
+// A start button that when clicked a timer starts and the first question appears.
+
+// Questions contain buttons for each answer.
+// When answer is clicked, the next question appears
+// If the answer clicked was incorrect then subtract time from the clock
+// The quiz should end when all questions are answered or the timer reaches 0.
+
+// When the game ends, it should display their score and give the user the ability to save their initials and their score
+
+
+// Tasks required
+
+// Start a timer when 'Start Quiz' button is clicked
+// 1 Need an event listener on the start quiz button click
+
+// Display the first question & multiple choice answers to the user
+// 1 need some questions and answers for user.
+// 2 could use an
+
+// Record which answer the user clicks on
+
+// Check if answer is correct
+//// if the answer is correct 'show correct' message and display next question
+///// Record score
+//// if the answer is wrong, subtract time from the timer & display next question
+
+// If all the questions are answered OR the timer runs out, end the quiz
+
+// Show the score to the user and let them save their result
